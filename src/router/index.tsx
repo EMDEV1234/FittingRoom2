08c810a32f67cbc179d270da5b338dd0e78cfebe
Your Name <your.email@example.com>
2025-07-28 14:13:25 -0400

    Restore all pages - complete website working

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -2,6 +2,14 @@
 import { createHashRouter } from "react-router-dom";
 import Layout from "../components/Layout";
 import Home from "../pages/Home";
+import Partners from "../pages/Partners";
+import TryOn from "../pages/TryOn";
+import Info from "../pages/Info";
+import About from "../pages/About";
+import Shopping from "../pages/Shopping";
+import Privacy from "../pages/Privacy";
+import Contact from "../pages/Contact";
+import ExampleProfile from "../pages/Profile/ExampleProfile";
 
 export const router = createHashRouter([
   {
@@ -11,6 +19,38 @@ export const router = createHashRouter([
       {
         index: true,
         element: <Home />,
+      },
+      {
+        path: "partners",
+        element: <Partners />,
+      },
+      {
+        path: "tryon",
+        element: <TryOn />,
+      },
+      {
+        path: "info",
+        element: <Info />,
+      },
+      {
+        path: "about",
+        element: <About />,
+      },
+      {
+        path: "shopping",
+        element: <Shopping />,
+      },
+      {
+        path: "privacy",
+        element: <Privacy />,
+      },
+      {
+        path: "contact",
+        element: <Contact />,
+      },
+      {
+        path: "profile",
+        element: <ExampleProfile />,
       },
     ],
   },
